9d9dfaa14d2b1dd01e78137b665e4d65855e6f2a
CsPauloNoob <ps616131@gmail.com>
2023-10-25 00:29:08 -0500

    corrigido connection string

--- Infrastructure/Data/Context/AppDbContext.cs
+++ Infrastructure/Data/Context/AppDbContext.cs
@@ -27,7 +27,7 @@ namespace Infrastructure.Data.Context
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlite("Data Source=D:\\home\\data");
+                optionsBuilder.UseSqlite("Data Source=C:\\home\\data\\Bot.db");
             }
         }
 
